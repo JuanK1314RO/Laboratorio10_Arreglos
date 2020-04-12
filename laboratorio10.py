61b72b045a9a16e0f776a330da6065ececb512fe
Juan Camilo Ramirez Ocampo <juan.ramirez.2020@upb.edu.co>
2020-04-12 18:56:03 -0500

    Modificacion con contador y paridad

--- laboratorio10.py
+++ laboratorio10.py
@@ -5,19 +5,28 @@ Created on Thu Apr  9 15:13:00 2020
 @author: Juan Camilo Ramirez
 """
 
-def a_power_b(a,b):
-   potencia = 1
-   while (b>0):
-       potencia = potencia*a
-       b = b-1
-   print("\nEl resulatado es:", potencia )
-   return potencia
+##funcion para calcular la potencia##
+def a_power_b(a, b):
+    potencia = 1
+    while (b>0):
+        potencia = potencia*a
+        b = b - 1
+    return potencia
 
-a=1
-while a<0 or a>0:
-    a = int(input('Ingrese base:  '))
-    b = abs(int(input('Ingrese exponente:  ')))
-    a_power_b(a,b)
-    
-a_power_b.count()
-print(a_power_b.count())
+##COntadores##
+a=1##Este es para empezar el ciclo while##
+cont_impares=0
+cont_pares=0
+cont=0
+while a!=0:
+    a = int(input("Ingrese base: "))
+    b = abs(int(input("Ingrese exponente: ")))
+    cont = cont + 1
+    if a_power_b(a, b)%2 != 0:
+       cont_impares += 1
+    else:
+        cont_pares += 1
+    print("El resultado es: "+str(a_power_b(a, b)))
+print("\nLa cantidad de impares es:", cont_impares)
+print("\nLa cantidad de pares es:", cont_pares)
+print("\nLa cantidad de potencias es:",cont)
