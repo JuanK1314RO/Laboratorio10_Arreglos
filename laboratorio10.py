903ba9ea7515ef63215157f2d84839a853380dbb
Juan Camilo Ramirez Ocampo <juan.ramirez.2020@upb.edu.co>
2020-04-12 13:06:03 -0500

    modificacion con multiples a de entrada

--- laboratorio10.py
+++ laboratorio10.py
@@ -5,16 +5,19 @@ Created on Thu Apr  9 15:13:00 2020
 @author: Juan Camilo Ramirez
 """
 
-##Asicacion de variables##
-a = int(input("Ingrese el valor de a: "))
-b = int(input("Ingrese el valor de la potencia: "))
-
 def a_power_b(a,b):
    potencia = 1
    while (b>0):
        potencia = potencia*a
        b = b-1
-       
+   print("\nEl resulatado es:", potencia )
    return potencia
 
-print("\nEl resulatado es:", a_power_b(a,b))
+a=1
+while a<0 or a>0:
+    a = int(input('Ingrese base:  '))
+    b = abs(int(input('Ingrese exponente:  ')))
+    a_power_b(a,b)
+    
+a_power_b.count()
+print(a_power_b.count())
